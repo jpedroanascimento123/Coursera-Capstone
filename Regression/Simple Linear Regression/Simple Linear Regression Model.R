bc19fd151133a2c0d7d45443b3f30a6a22ffaba8
jpedroanascimento123 <45755173+jpedroanascimento123@users.noreply.github.com>
2018-12-13 17:25:08 +0000

    Add files via upload

---
+++ Regression/Simple Linear Regression/Simple Linear Regression Model.R
@@ -0,0 +1,53 @@
+# Import the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training and Test set (one to teach our model, the other to test it)
+# The dependant variable is Purchased 
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary,
+                     SplitRatio = 2/3)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+# Feature scaling (putting our values on the same scale - all except categorical columns)
+training_set[,1:2] = scale(training_set[, 1:2])
+test_set[,1:2] = scale(test_set[, 1:2])
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience, 
+               data = training_set)
+
+# Get some info regarding the regressor. Looking at the coefficients, we can see that the independant variable YearsExperience has a strong effect on our dependant variable (p-value extremely low)
+summary(regressor)
+
+# Predicting the Test set results
+predictor = predict(regressor, newdata = test_set)
+
+# Visualising the Training set results
+install.packages('ggplot2')
+ggplot() + 
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = "red") + 
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = "blue") + 
+  ggtitle('Salary vs Experience (Training set)') + 
+  xlab('Years of Experience') + 
+  ylab('Salary')
+
+
+# Visualising the Test set results
+install.packages('ggplot2')
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = "red") + 
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = "blue") + 
+  ggtitle('Salary vs Experience (Test set)') + 
+  xlab('Years of Experience') + 
+  ylab('Salary')
+
+# Predicting some results
+predictor = predict(regressor, data.frame(YearsExperience = 8))
+predictor = predict(regressor, data.frame(YearsExperience = 20))
